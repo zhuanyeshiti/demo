0f2dbc5cb011087066604cb92e9f764124a5a5ec
zhuanyeshiti <liuhongwei123456@gmail.com>
2016-12-16 09:42:06 +0800

    add some files

--- t1.c
+++ t1.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define OK(i)	if(i==5) continue
 
 int main()
 {
+	char *s;
+	char *p;
+	s=malloc(1024);
+	p=s;
+	*p='b';
+	*s='a';
+	//s="abcdefg";
 	int i;
-	i=0;
-	if(getchar()=='a'&&++i)
+	for(i=0;i<10;i++)
 	{
-		getchar();
-		printf("good: %d\n",i);
+		printf("%d\n",i);
+		OK(i);
+	//	if(i==5)
+	//		continue;
+		printf("hello!\n");
 	}
-	else
-	{
-		getchar();
-		printf("good: %d\n",i);
-	}
+	//printf("%c\n",*s);
+	printf("%s\n",p);
+	printf("%s\n",s);
+	printf("%c\n",*p);
+	printf("%p\n",p);
+	printf("%p\n",s);
 	return 0;
 }
