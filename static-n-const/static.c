2bbd0bc57a5cab90e9ada79312b29597da5d5fd4
zhuanyeshiti <1181052146@qq.com>
2016-12-26 18:38:32 +0800

    modify files

--- static-n-const/static.c
+++ static-n-const/static.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 extern int hello();
 int a;
 //static int a;
+static const int c=1;
 
 int main()
 {
 	//static int a;
-	//int a;
+	int *d;
+	d=(int *)malloc(sizeof(int));
+	*d=c;
 	a=1;
 	printf("static a: %d\n",a);
 	hello();
+	*d=2;
+	printf("%d\n",*d);
 	return 0;
 }
