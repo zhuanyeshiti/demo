1dbdc8fe127f880914be66ee2b5a9d96d48319b5
zhuanyeshiti <liuhongwei123456@gmail.com>
2017-01-08 00:15:45 +0800

    modify

--- socket-n-shell/server/command.c
+++ socket-n-shell/server/command.c
@@ -5,7 +5,10 @@ int command(char *command)
 	int a;
 	char buffer[200]={0};
 	//char *command;
-	command="lllllll";
+	//command="lllllll";
+	if(command==NULL)
+		return -1;
+
 	FILE *fp;
 
 	fp=popen(command,"r");
