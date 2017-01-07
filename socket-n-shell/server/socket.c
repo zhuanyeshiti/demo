891ba08f4f31e51621a1ed469109de454cf6851e
zhuanyeshiti <liuhongwei123456@gmail.com>
2017-01-08 00:56:39 +0800

    modify

--- socket-n-shell/server/socket.c
+++ socket-n-shell/server/socket.c
@@ -1,9 +1,48 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
 
-extern int command(char *cmd);
+//extern int command(char *cmd);
+
+void *thread_func(void *arg);
+char *message;
 
 int main()
 {
-	command("ls");
+	int result;
+	pthread_t a_thread;
+	void *thread_result;
+	message="hello!";
+	result=pthread_create(&a_thread,NULL,thread_func,(void *)message);
+	if(result!=0)
+	{
+		printf("something wrong!\n");
+		exit(0);
+	}
+
+	result=pthread_join(a_thread,&thread_result);
+	if(result!=0)
+	{
+		printf("something wrong with join!\n");
+		exit(0);
+	}
+
+	printf("thread join and return %s\n",(char *)thread_result);
+
+	//command("ls");
 	return 0;
 }
+
+
+void *thread_func(void *arg)
+{
+//	command("ls");
+
+//	pthread_exit("thread done!\n");
+}
+
+
+
+
