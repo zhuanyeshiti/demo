b9271cfcd6397678a5fe5ca6ca558b93cca513e8
zhuanyeshiti <liuhongwei123456@gmail.com>
2017-05-30 14:55:46 +0000

    add

--- tty/mpty.c
+++ tty/mpty.c
@@ -63,6 +63,7 @@ int main(int argc, char *argv[])
 		 */
 
 		char * temp3;
+		char temp4[200] = {'\0'};
 		fpid = fork();
 		if(fpid<0)
 		{
@@ -73,24 +74,25 @@ int main(int argc, char *argv[])
 			dup2(spty, STDIN_FILENO);
 			dup2(spty, STDOUT_FILENO);
 			//dup2(mpty, STDERR_FILENO);
-			//close(mpty);
+			close(mpty);
 			printf("child\n");
-			execl("/bin/sh", "sh", "-c", "vim file", (char *)0);
-			//execl("/bin/sh", "sh", "-c", "ls", (char *)0);
+			//execl("/bin/sh", "sh", "-c", "vim file", (char *)0);
+			execl("/bin/sh", "sh", "-c", "ls", (char *)0);
 			//c = read(spty, temp3, strlen(temp3));
 		}
 		else
 		{	
-			temp3 = ":wq\n";
+			//temp3 = ":wq\n";
+			//c = write(mpty, temp3, strlen(temp));
 			sleep(1);
-			c = write(mpty, temp3, strlen(temp));
-			sleep(1);
-			//while(c = read(mpty, temp2, 100))
-			//{
-				c = read(mpty, temp3, strlen(temp3));
+			while(c = read(mpty, temp4, 200))
+			{
+				//c = read(mpty, temp3, 100);
+				//must use char temp4 = {'\0'};
+				//instead of char * temp3;
 				printf("parent.\n");
-				printf("%s", temp3);
-			//}
+				printf("%s", temp4);
+			}
 			//sleep(1);
 			//execl("/bin/sh", "sh", "-c", "ls -l", (char *)0);
 		}
